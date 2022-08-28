72a740decb615b86088499a8d76a90840cf5714b
Dmitrii Grigorev <ploytary.fed@gmail.com>
2022-08-28 20:41:54 +0300

    fix: fix img links

--- assets/Ancients/index.js
+++ assets/Ancients/index.js
@@ -1,8 +1,8 @@
 const ancients = {
-  azathoth: '/assets/Ancients/Azathoth.jpg',
-  cthulhu: '/assets/Ancients/Cthulthu.jpg',
-  iogSothoth: '/assets/Ancients/IogSothoth.jpg',
-  shubNiggurath: '/assets/Ancients/ShubNiggurath.jpg'
+  azathoth: './assets/Ancients/Azathoth.jpg',
+  cthulhu: './assets/Ancients/Cthulthu.jpg',
+  iogSothoth: './assets/Ancients/IogSothoth.jpg',
+  shubNiggurath: './assets/Ancients/ShubNiggurath.jpg'
 }
 
 export default ancients
